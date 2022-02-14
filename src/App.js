06a37b1f124a3aa5acf9926877a18dc205a5d239
Your Name <you@example.com>
2022-02-14 06:54:51 +0500

    Added header component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 const HatsPage = () => (
   <div>
@@ -12,10 +13,13 @@ const HatsPage = () => (
 
 function App() {
   return (
+    <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
       </Switch>
+    </div>
   );
 }
 
